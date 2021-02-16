fdc277d0323b973f4d6e88b8ce221a1ad2ea149e
yuanyuan_wang <yuanyuan.wang@airbnb.com>
2021-02-16 22:18:09 +0800

    init

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Yuanyuan Wang on 2021/2/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SquareDancingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquareDancingApp.swift
+//  Shared
+//
+//  Created by Yuanyuan Wang on 2021/2/16.
+//
+
+import SwiftUI
+
+@main
+struct SquareDancingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
